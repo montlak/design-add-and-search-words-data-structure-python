b31c1ab88e74c95ed654bf0cca7d69eac217d8c6
Jonatthan Rafael Zazueta Rivera <jonatthanrafaelzazuetarivera@minidejonatthan.localdomain>
2020-09-23 16:41:52 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+class WordDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.structure = []
+        
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+        self.structure.append(word)
+        
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        """
+        result = False
+        if word in self.structure:
+            result = True
+        else:
+            for item in self.structure:
+                if len(word) == len(item):
+                    for i in range(len(word)):
+                        if word[i] != ".":
+                            if word[i] == item[i]:
+                                result = True
+                            else:
+                                result = False
+                                break
+                if result:
+                    break
+        return result
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
